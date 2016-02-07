82088402d54a9ac6d6d99a125e3b57c72ff7e846
Andrew <andrew.carroll1@gmail.com>
2016-02-06 22:57:06 -0500

    reset back to earlier

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,10 @@
-console.log('This would be the main JS file.');
+(function () {
+    cheet('U U D D L R L R b a', function () {
+        var video = '<iframe width="560" height="315" src="https://www.youtube.com/embed/q5SZzFaAZyY" frameborder="0" allowfullscreen></iframe>';
+
+        $('iframe').remove()
+
+        $('body').append(video);
+
+    });
+})();
